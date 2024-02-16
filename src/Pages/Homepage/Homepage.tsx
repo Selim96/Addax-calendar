e9ef7d6b8deff91323edbd9e9222b283b05cad63
Selim <93911103+Selim96@users.noreply.github.com>
2024-02-16 14:33:47 +0200

    fix drag

--- src/Pages/Homepage/Homepage.tsx
+++ src/Pages/Homepage/Homepage.tsx
@@ -54,7 +54,8 @@ const Homepage: React.FC = () => {
             const newItems = day.items.filter(item=>item.id !== currentCard.id);
             const newDay = {...day, items: newItems}
 
-            const currentIndex = currentBoard.items.indexOf(currentCard);
+            const currentIndex = currentBoard.items.findIndex(item=>item.id=== currentCard.id);
+            console.log(currentIndex)
             currentBoard?.items.splice(currentIndex, 1);
             const dropIndex = newDay.items.findIndex(elem=> elem.id === onDropItemId);
             newDay.items.splice(dropIndex+1, 0, currentCard);
